cd621fa45fa64e123ca630a4fc60cd0fd2c9f9c9
Kenrick Beckett <kenrick.beckett@gmail.com>
2014-03-11 21:29:09 -0500

    Update tweet decorator to using twitter handles for the trader It now also shows the difference in the amount for the rate change

--- lib/tweet_decorator.rb
+++ lib/tweet_decorator.rb
@@ -1,39 +1,66 @@
 class TweetDecorator < Struct.new(:current, :previous, :rate_type)
 
   HUMAN_RATE_TYPES = {
-    buy_cash: "buying cash",
-    buy_draft: "buying draft",
-    sell_cash: "selling cash",
-    sell_draft: "selling draft"
+    buy_cash: "cash buying",
+    buy_draft: "draft buying",
+    sell_cash: "cash selling",
+    sell_draft: "draft selling"
+  }
+
+  TRADER_TWITTER_HANDERS = {
+    "NCB" => "@ncbja",
+    "JMMB" => "@JMMBGROUP",
+    "BNS" => "@ScotiabankJM",
+    "JNBS" => "@JamaicaNational",
+    "FGB" => "@First_Global",
+    "Sagicore" => "@SagicorJa",
   }
 
   def build_tweet
-    "#{trader_name} has changed their #{note} #{human_rate_type} rate from $#{previous_amount} to $#{current_amount}"
+    "#{trader_handle} has changed their #{human_rate_type} #{note_hash_tag} rate #{movement} from $#{previous_amount} to $#{current_amount}. (#{amount_difference})"
   end
 
   private
 
-  def trader_name
-    current.trader.name
+  def amount_difference
+    amount = bg_to_money(difference)
+    amount.insert(0, "+") if difference > 0
+    amount
+  end
+
+  def trader_handle
+    TRADER_TWITTER_HANDERS[trader_short_name] || trader_short_name
+  end
+
+  def trader_short_name
+    current.trader.short_name
   end
 
   def human_rate_type
     HUMAN_RATE_TYPES[rate_type]
   end
 
-  def note
-    current.note.upcase
+  def note_hash_tag
+    "##{current.note.upcase}"
   end
 
   def previous_amount
-    bg_to_money(previous)
+    bg_to_money(previous.send(rate_type))
   end
 
   def current_amount
-    bg_to_money(current)
+    bg_to_money(current.send(rate_type))
   end
 
-  def bg_to_money(currency)
-    "%.2f" % currency.send(rate_type).to_s("F")
+  def difference
+    current.send(rate_type) - previous.send(rate_type)
+  end
+
+  def movement
+    difference > 0 ? "#up" : "#down"
+  end
+
+  def bg_to_money(bg)
+    "%.2f" % bg.to_s("F")
   end
 end
--- spec/tweet_decorator_spec.rb
+++ spec/tweet_decorator_spec.rb
@@ -1,16 +1,30 @@
 require 'spec_helper'
 
 describe TweetDecorator do
-  let(:trader) { double(name: "National Bank") }
+  let(:trader) { double(short_name: "NCB") }
   let(:previous) { double(trader: trader, note: "usd", buy_cash: BigDecimal.new("106.10")) }
   let(:current) { double(trader: trader, note: "usd", buy_cash: BigDecimal.new("106.40")) }
   let(:rate_type) { :buy_cash }
-  let(:decorator) { TweetDecorator.new(current, previous, rate_type) }
+  subject(:decorator) { TweetDecorator.new(current, previous, rate_type) }
 
   describe "#build_tweet" do
 
-    it "should return a structured comparison string" do
-      decorator.build_tweet.should eq("National Bank has changed their USD buying cash rate from $106.10 to $106.40")
+    context "given the buy_cash rate increase for NCB" do
+      it "should return a structured comparison string" do
+        decorator.build_tweet.should eq("@ncbja has changed their cash buying #USD rate #up from $106.10 to $106.40. (+0.30)")
+      end
+    end
+
+    context "given the sell_cash rate decreased for FXTRADERS" do
+
+      let(:trader) { double(short_name: "FXTRADERS") }
+      let(:previous) { double(trader: trader, note: "usd", sell_cash: BigDecimal.new("108.25")) }
+      let(:current) { double(trader: trader, note: "usd", sell_cash: BigDecimal.new("107.00")) }
+      let(:rate_type) { :sell_cash }
+
+      it "should return a structured comparison string" do
+        decorator.build_tweet.should eq("FXTRADERS has changed their cash selling #USD rate #down from $108.25 to $107.00. (-1.25)")
+      end
     end
 
   end
